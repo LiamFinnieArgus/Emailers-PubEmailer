2eeff5474c39023f711e06b9e555503f4596d9b0
liam.finnie <liam.finnie@LON-AL00649>
2020-08-06 12:02:58 +0100

    Initial commit.

---
+++ src/Argus.Emailers.PubEmailer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Argus.Emailers.PubEmailer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
